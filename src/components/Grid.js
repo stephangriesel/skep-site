7487b004586101c7b49b8ae1c9e84bc29ada7bd0
Steve <sgriesel@gmail.com>
2019-05-29 15:26:27 +0200

    basic set up & grid

---
+++ src/components/Grid.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import '../css/Grid.css';
+
+class Grid extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <main className="card">
+          <div className="card">
+            <div className="info">
+              <strong>Title</strong>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+            </div>
+            <img src="https://loremflickr.com/320/240/bordercollie" alt="dogimage" />
+          </div>
+          <div className="card">
+            <div className="info">
+              <strong>Title</strong>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+            </div>
+            <img src="https://loremflickr.com/320/240/jackrussel" alt="dogimage" />
+          </div>
+          <div className="card">
+            <div className="info">
+              <strong>Title</strong>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+            </div>
+            <img src="https://loremflickr.com/320/240/poodle" alt="dogimage" />
+          </div>
+          <div className="card">
+            <div className="info">
+              <strong>Title</strong>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+            </div>
+            <img src="https://loremflickr.com/320/240/greyhound" alt="dogimage" />
+          </div>
+          <div className="card">
+            <div className="info">
+              <strong>Title</strong>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+            </div>
+            <img src="https://loremflickr.com/320/240/doberman" alt="dogimage" />
+          </div>
+          <div className="card">
+            <div className="info">
+              <strong>Title</strong>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+            </div>
+            <img src="https://loremflickr.com/320/240/dalmation" alt="dogimage" />
+          </div>
+          <div className="card">
+            <div className="info">
+              <strong>Title</strong>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+            </div>
+            <img src="https://loremflickr.com/320/240/sharpei" alt="dogimage" />
+          </div>
+          <div className="card">
+            <div className="info">
+              <strong>Title</strong>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+            </div>
+            <img src="https://loremflickr.com/320/240/pug" alt="dogimage" />
+          </div>
+          <div className="card">
+            <div className="info">
+              <strong>Title</strong>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+            </div>
+            <img src="https://loremflickr.com/320/240/frenchbulldog" alt="dogimage" />
+          </div>
+          <div className="card">
+            <div className="info">
+              <strong>Title</strong>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+            </div>
+            <img src="https://loremflickr.com/320/240/germanshepherd" alt="dogimage" />
+          </div>
+        </main>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Grid;
