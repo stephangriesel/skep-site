b527c2987c3841bd1b180bda167b28588b259356
Stephan <sgriesel@gmail.com>
2019-06-23 13:53:28 +0200

    update logic, add card, update styles

--- src/components/projects/ProjectOne.js
+++ src/components/projects/ProjectOne.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
 import '../../css/ImageSlider.css';
+import data from '../../data/ProjectOneData';
+import Card from '../Card';
 
 
 class ProjectOne extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            properties: data.properties,
+            property: data.properties[0]
+        }
+    }
+
+    nextProperty = () => {
+        const newIndex = this.state.property.index + 1;
+        this.setState({
+            property: data.properties[newIndex]
+        })
+    }
+
+    prevProperty = () => {
+        const newIndex = this.state.property.index - 1;
+        this.setState({
+            property: data.properties[newIndex]
+        })
+    }
+
     render() {
+        const { property } = this.state;
         return (
             <div className="image-slider">
                 <div className="cards-slider">
                     <div className="card-slider-wrapper">
-                        <h1>Project one</h1>
+                        <Card property={property} />
                     </div>
                 </div>
+                <button
+                    onClick={() => this.nextProperty()}
+                    disabled={property.index === data.properties.length - 1}
+                >Next</button>
+                <button
+                    onClick={() => this.prevProperty()}
+                    disabled={property.index === 0}
+                >Prev</button>
             </div>
+
         )
     }
 }
