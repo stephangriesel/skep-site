7c488196629ee00eaf4181e3e1c71435bb1cd37c
Stephan <sgriesel@gmail.com>
2019-06-24 16:18:18 +0200

    add shop page

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,11 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import '../css/Shop.css';
 
 class Shop extends Component {
     render() {
         return (
-            <div>
-                <h1>Shop Page</h1>
-            </div>
+            <React.Fragment>
+            <main className="shop-card">
+                <div className="shop-card">
+                    <a href="/projectone">
+                        <img
+                            src="https://loremflickr.com/320/240/woman"
+                            alt="dogimage"
+                        />
+                    </a>
+                    <div className="info">
+                        <strong>Title</strong>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                    </div>
+                </div>
+                <div className="shop-card">
+                    <a href="/projectone">
+                        <img
+                            src="https://loremflickr.com/320/240/man"
+                            alt="dogimage"
+                        />
+                    </a>
+                    <div className="info">
+                        <strong>Title</strong>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                    </div>
+                </div>
+                <div className="shop-card">
+                    <a href="/projectone">
+                        <img
+                            src="https://loremflickr.com/320/240/lady"
+                            alt="dogimage"
+                        />
+                    </a>
+                    <div className="info">
+                        <strong>Title</strong>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                    </div>
+                </div>
+            </main>
+        </React.Fragment>
+
         )
     }
 }
