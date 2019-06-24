fe1fec3e2c0e7858b644a2f538a47f2dabc4f2c8
Stephan <sgriesel@gmail.com>
2019-06-24 15:10:15 +0200

    add office page

--- src/components/Office.js
+++ src/components/Office.js
@@ -1,13 +1,254 @@
 import React, { Component } from 'react';
+import '../css/Office.css';
 
 class Office extends Component {
     render() {
         return (
-            <div>
-                <h1>Office Page</h1>
+            <div className="center-wrapper">
+                <header className="office-office">
+                    <h1>Office</h1>
+                </header>
+                <section className="flex-twocolumns">
+                    <div className="section-column">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida tempus sollicitudin. Suspendisse potenti. Phasellus vehicula ligula a elit venenatis, eget auctor tortor maximus. Suspendisse vel dolor et nibh posuere volutpat eget quis augue. Morbi blandit mi non auctor tempus. Ut elementum orci sed tortor fringilla, vel rutrum justo pharetra. Sed a odio non velit varius blandit. Suspendisse potenti. Praesent blandit commodo risus eget convallis. Aliquam a est nec nibh lobortis venenatis in ut sem. Quisque non felis libero.</p>
+                    </div>
+                    <div className="section-column">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida tempus sollicitudin. Suspendisse potenti. Phasellus vehicula ligula a elit venenatis, eget auctor tortor maximus. Suspendisse vel dolor et nibh posuere volutpat eget quis augue. Morbi blandit mi non auctor tempus. Ut elementum orci sed tortor fringilla, vel rutrum justo pharetra. Sed a odio non velit varius blandit. Suspendisse potenti. Praesent blandit commodo risus eget convallis. Aliquam a est nec nibh lobortis venenatis in ut sem. Quisque non felis libero.</p>
+                    </div>
+                </section>
+                <section>
+                    <div className="map-section">
+
+                    </div>
+                </section>
+                <section className="flex-fourcolumns">
+                    <div>
+                        <h3>Address</h3>
+                        <p>Some text yes please</p>
+                        <p>Some more text</p>
+                        <p>Maybe a phone number</p>
+                        <p>And email address: test@test.com</p>
+                    </div>
+                    <div>
+                        <h3>Address</h3>
+                        <p>Some text yes please</p>
+                        <p>Some more text</p>
+                        <p>Maybe a phone number</p>
+                        <p>And email address: test@test.com</p>
+                    </div>
+                    <div>
+                        <h3>Address</h3>
+                        <p>Some text yes please</p>
+                        <p>Some more text</p>
+                        <p>Maybe a phone number</p>
+                        <p>And email address: test@test.com</p>
+                    </div>
+                    <div>
+                        <h3>Address</h3>
+                        <p>Some text yes please</p>
+                        <p>Some more text</p>
+                        <p>Maybe a phone number</p>
+                        <p>And email address: test@test.com</p>
+                    </div>
+                </section>
+                <header className="office-people">
+                    <h1>People</h1>
+                </header>
+                <React.Fragment>
+                    <main className="office-card">
+                        <div className="office-card">
+                            <div className="info">
+                                <strong>Title</strong>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                            </div>
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/woman"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="office-card">
+                            <div className="info">
+                                <strong>Title</strong>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                            </div>
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/man"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="office-card">
+                            <div className="info">
+                                <strong>Title</strong>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                            </div>
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/lady"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                    </main>
+                </React.Fragment>
+                <header className="office-clients">
+                    <h1>Clients</h1>
+                </header>
+                <React.Fragment>
+                    <main className="client-card">
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/woman"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/man"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/lady"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/woman"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/man"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/lady"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/woman"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/man"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/lady"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/woman"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/man"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/lady"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/woman"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/man"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/lady"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/woman"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/man"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                        <div className="client-card">
+                            <a href="/projectone">
+                                <img
+                                    src="https://loremflickr.com/320/240/lady"
+                                    alt="dogimage"
+                                />
+                            </a>
+                        </div>
+                    </main>
+                </React.Fragment>
             </div>
         )
     }
 }
 
 export default Office;
+
