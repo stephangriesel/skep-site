e9d69c2318121ba845abecea0d5745b41e8bc1ce
Stephan <sgriesel@gmail.com>
2019-06-25 13:48:57 +0200

    update contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,26 +8,23 @@ class Contact extends Component {
                 <header className="contact-contact">
                     <h1>Contact</h1>
                 </header>
-                <form name="contact" action="POST" data-netlify="true">
+                <form method="post">
+                    <input type="hidden" name="form-name" value="contact" />
                     <p>
-                        <input type="text" name="name" placeholder="Please enter your name" />
+                        <label>
+                            Your Name: <input type="text" name="name" />
+                        </label>
                     </p>
                     <p>
-                        <input type="email" name="email" placeholder="Your Email Address"/>
+                        <label>
+                            Your Email: <input type="email" name="email" />
+                        </label>
                     </p>
                     <p>
-                        <input type="tel" name="number" placeholder="Contact Number"/>
+                        <label>
+                            Message: <textarea name="message" />
+                        </label>
                     </p>
-                    <p>
-                        <select name="role[]" multiple>
-                            <option value="leader">Feedback</option>
-                            <option value="follower">Shop Enquiry</option>
-                        </select>
-                    </p>
-                    <p>
-                        <textarea name="message" placeholder="Your Message"></textarea>
-                    </p>
-                    <div data-netlify-recaptcha="true"></div>
                     <p>
                         <button type="submit">Send</button>
                     </p>
