02e0229393e85cb0e01d5bdf1ad834c3a697e4bd
Stephan <sgriesel@gmail.com>
2019-09-15 12:04:13 +0200

    add shop items

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,50 +1,184 @@
 import React, { Component } from 'react';
 import '../css/Shop.css';
+import shop1 from '../img/shop/shop1.jpg';
+import shop2 from '../img/shop/shop2.jpg';
+import shop3 from '../img/shop/shop3.jpg';
+import shop4 from '../img/shop/shop4.jpg';
+import shop5 from '../img/shop/shop5.jpg';
+import shop6 from '../img/shop/shop6.jpg';
+import shop7 from '../img/shop/shop7.jpg';
+import shop8 from '../img/shop/shop8.jpg';
+import shop9 from '../img/shop/shop9.jpg';
+import shop10 from '../img/shop/shop10.jpg';
+import shop11 from '../img/shop/shop11.jpg';
+import shop12 from '../img/shop/shop12.jpg';
+import shop13 from '../img/shop/shop13.jpg';
+import shop14 from '../img/shop/shop14.jpg';
 
 class Shop extends Component {
     render() {
         return (
             <React.Fragment>
-            <main className="shop-card">
-                <div className="shop-card">
-                    <a href="/projectone">
-                        <img
-                            src="https://loremflickr.com/320/240/woman"
-                            alt="dogimage"
-                        />
-                    </a>
-                    <div className="info">
-                        <strong>Title</strong>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                <main className="shop-card">
+                    <div className="shop-card">
+                        <a href="/projectone">
+                            <img
+                                src={shop1}
+                                alt="dogimage"
+                            />
+                        </a>
+                        <div className="info">
+                            <strong>Title</strong>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                        </div>
                     </div>
-                </div>
-                <div className="shop-card">
-                    <a href="/projectone">
-                        <img
-                            src="https://loremflickr.com/320/240/man"
-                            alt="dogimage"
-                        />
-                    </a>
-                    <div className="info">
-                        <strong>Title</strong>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                    <div className="shop-card">
+                        <a href="/projectone">
+                            <img
+                                src={shop2}
+                                alt="dogimage"
+                            />
+                        </a>
+                        <div className="info">
+                            <strong>Title</strong>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                        </div>
                     </div>
-                </div>
-                <div className="shop-card">
-                    <a href="/projectone">
-                        <img
-                            src="https://loremflickr.com/320/240/lady"
-                            alt="dogimage"
-                        />
-                    </a>
-                    <div className="info">
-                        <strong>Title</strong>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                    <div className="shop-card">
+                        <a href="/projectone">
+                            <img
+                                src={shop3}
+                                alt="dogimage"
+                            />
+                        </a>
+                        <div className="info">
+                            <strong>Title</strong>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                        </div>
                     </div>
-                </div>
-            </main>
-        </React.Fragment>
+                    <div className="shop-card">
+                        <a href="/projectone">
+                            <img
+                                src={shop4}
+                                alt="dogimage"
+                            />
+                        </a>
+                        <div className="info">
+                            <strong>Title</strong>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                        </div>
+                    </div>
+                    <div className="shop-card">
+                        <a href="/projectone">
+                            <img
+                                src={shop5}
+                                alt="dogimage"
+                            />
+                        </a>
+                        <div className="info">
+                            <strong>Title</strong>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                        </div>
+                    </div>
+                    <div className="shop-card">
+                        <a href="/projectone">
+                            <img
+                                src={shop6}
+                                alt="dogimage"
+                            />
+                        </a>
+                        <div className="info">
+                            <strong>Title</strong>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                        </div>
+                    </div>
+                    <div className="shop-card">
+                        <a href="/projectone">
+                            <img
+                                src={shop7}
+                                alt="dogimage"
+                            />
+                        </a>
+                        <div className="info">
+                            <strong>Title</strong>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                        </div>
+                    </div>
+                    <div className="shop-card">
+                        <a href="/projectone">
+                            <img
+                                src={shop8}
+                                alt="dogimage"
+                            />
+                        </a>
+                        <div className="info">
+                            <strong>Title</strong>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                        </div>
+                    </div>
+                    <div className="shop-card">
+                        <a href="/projectone">
+                            <img
+                                src={shop10}
+                                alt="dogimage"
+                            />
+                        </a>
+                        <div className="info">
+                            <strong>Title</strong>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                        </div>
+                    </div>
+                    <div className="shop-card">
+                        <a href="/projectone">
+                            <img
+                                src={shop11}
+                                alt="dogimage"
+                            />
+                        </a>
+                        <div className="info">
+                            <strong>Title</strong>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                        </div>
+                    </div>
+                    <div className="shop-card">
+                        <a href="/projectone">
+                            <img
+                                src={shop12}
+                                alt="dogimage"
+                            />
+                        </a>
+                        <div className="info">
+                            <strong>Title</strong>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                        </div>
+                    </div>
+                    <div className="shop-card">
+                        <a href="/projectone">
+                            <img
+                                src={shop13}
+                                alt="dogimage"
+                            />
+                        </a>
+                        <div className="info">
+                            <strong>Title</strong>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                        </div>
+                    </div>
+                    <div className="shop-card">
+                        <a href="/projectone">
+                            <img
+                                src={shop14}
+                                alt="dogimage"
+                            />
+                        </a>
+                        <div className="info">
+                            <strong>Title</strong>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                        </div>
+                    </div>
+                </main>
+            </React.Fragment>
 
         )
     }
