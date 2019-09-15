27c3ab2d91c0299780b36fa692ccac5a4490788f
Stephan <sgriesel@gmail.com>
2019-09-15 06:49:23 +0200

    add team

--- src/components/Office.js
+++ src/components/Office.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import '../css/Office.css';
+import geraldine from '../img/office/geraldine.jpg';
+import carla from '../img/office/carla.jpg';
+import charlotte from '../img/office/charlotte.jpg';
+import christi from '../img/office/christi.jpg';
+import gunther from '../img/office/gunther.jpg';
+import kulsum from '../img/office/kulsum.jpg';
+import lumari from '../img/office/lumari.jpg';
 
 class Office extends Component {
     render() {
@@ -10,45 +17,31 @@ class Office extends Component {
                 </header>
                 <section className="flex-twocolumns">
                     <div className="section-column">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida tempus sollicitudin. Suspendisse potenti. Phasellus vehicula ligula a elit venenatis, eget auctor tortor maximus. Suspendisse vel dolor et nibh posuere volutpat eget quis augue. Morbi blandit mi non auctor tempus. Ut elementum orci sed tortor fringilla, vel rutrum justo pharetra. Sed a odio non velit varius blandit. Suspendisse potenti. Praesent blandit commodo risus eget convallis. Aliquam a est nec nibh lobortis venenatis in ut sem. Quisque non felis libero.</p>
+                        <p>‘SKEP’ – to produce through artistic or imaginative effort.</p>
+                        <p>SKEP offers solutions tailored to specific needs and style, striving to produce creative, honest and environmentally responsible designs that add value to investments.</p>
                     </div>
                     <div className="section-column">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida tempus sollicitudin. Suspendisse potenti. Phasellus vehicula ligula a elit venenatis, eget auctor tortor maximus. Suspendisse vel dolor et nibh posuere volutpat eget quis augue. Morbi blandit mi non auctor tempus. Ut elementum orci sed tortor fringilla, vel rutrum justo pharetra. Sed a odio non velit varius blandit. Suspendisse potenti. Praesent blandit commodo risus eget convallis. Aliquam a est nec nibh lobortis venenatis in ut sem. Quisque non felis libero.</p>
+                        <p>SKEP is a Stellenbosch based firm specializing in architecture and interior design services.SKEP is a Stellenbosch based firm specializing in architecture and interior design services.</p>
+                        <p>Founded in 2012 by Carla Gerber and Geraldine Heyns.</p>
                     </div>
                 </section>
                 <section>
-                    <div className="map-section">
-
+                    <div className="">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3310.1611883890564!2d18.8581432157708!3d-33.93698213003329!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcdb25e904f9435%3A0x3f25de2c883a3026!2s4%20Plein%20St%2C%20Stellenbosch%20Central%2C%20Stellenbosch%2C%207600%2C%20South%20Africa!5e0!3m2!1sen!2snl!4v1568519891475!5m2!1sen!2snl" width="100%" height="700" frameborder="0" allowfullscreen="" title="map"></iframe>
                     </div>
                 </section>
-                <section className="flex-fourcolumns">
-                    <div>
-                        <h3>Address</h3>
-                        <p>Some text yes please</p>
-                        <p>Some more text</p>
-                        <p>Maybe a phone number</p>
-                        <p>And email address: test@test.com</p>
+                <section className="flex-twocolumns">
+                    <div className="section-column">
+                        <h3>Street Address</h3>
+                        <p>2nd Floor</p>
+                        <p>Plein Street 4</p>
+                        <p>Anglo African Building</p>
+                        <p>Stellenbosch, 7600</p>
                     </div>
-                    <div>
-                        <h3>Address</h3>
-                        <p>Some text yes please</p>
-                        <p>Some more text</p>
-                        <p>Maybe a phone number</p>
-                        <p>And email address: test@test.com</p>
-                    </div>
-                    <div>
-                        <h3>Address</h3>
-                        <p>Some text yes please</p>
-                        <p>Some more text</p>
-                        <p>Maybe a phone number</p>
-                        <p>And email address: test@test.com</p>
-                    </div>
-                    <div>
-                        <h3>Address</h3>
-                        <p>Some text yes please</p>
-                        <p>Some more text</p>
-                        <p>Maybe a phone number</p>
-                        <p>And email address: test@test.com</p>
+                    <div className="section-column">
+                        <h3>P.O. Box Address</h3>
+                        <p>P.O. Box 396</p>
+                        <p>Stellenbosch, 7599</p>
                     </div>
                 </section>
                 <header className="office-people">
@@ -57,40 +50,94 @@ class Office extends Component {
                 <React.Fragment>
                     <main className="office-card">
                         <div className="office-card">
+                            <img
+                                src={geraldine}
+                                alt="Geraldine"
+                            />
                             <div className="info">
-                                <strong>Title</strong>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                                <strong>Geraldine Heyns</strong>
+                                <h5><em>Architect and interior designer</em></h5>
+                                <p>Graduated with a M Arch and BSc Arch from the University of Pretoria and completed her MBA from UNISA in 2011</p>
+                                <p>Loves: Her kids and the colour yellow</p>
+                                <p>Superpowers: Smiling for days <span style={{ fontSize: '28px' }}>☺</span></p>
                             </div>
-                            <a href="/projectone">
-                                <img
-                                    src="https://loremflickr.com/320/240/woman"
-                                    alt="dogimage"
-                                />
-                            </a>
                         </div>
                         <div className="office-card">
+                            <img
+                                src={carla}
+                                alt="Carla"
+                            />
                             <div className="info">
-                                <strong>Title</strong>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                                <strong>Carla Gerber</strong>
+                                <h5><em>Interior designer</em></h5>
+                                <p>Graduated with a Higher Diploma from The Design School of South Africa – affiliated with London School of arts</p>
+                                <p>Loves: family & good wine</p>
+                                <p>Superpowers: Always sees the good in people</p>
                             </div>
-                            <a href="/projectone">
-                                <img
-                                    src="https://loremflickr.com/320/240/man"
-                                    alt="dogimage"
-                                />
-                            </a>
                         </div>
                         <div className="office-card">
+                            <img
+                                src={charlotte}
+                                alt="Charlotte"
+                            />
                             <div className="info">
-                                <strong>Title</strong>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus similique explicabo voluptates, quod nostrum? Obcaecati incidunt adipisci aut quaerat?</p>
+                                <strong>Carla Gerber</strong>
+                                <h5><em>Interior designer</em></h5>
+                                <p>Graduated with an interior design diploma from Design Time School of Interior Design in 2011</p>
+                                <p>Loves: All animals, Cinnabuns & my husband</p>
+                                <p>Superpowers: I can spring clean a house in 15 minute</p>
                             </div>
-                            <a href="/projectone">
-                                <img
-                                    src="https://loremflickr.com/320/240/lady"
-                                    alt="dogimage"
-                                />
-                            </a>
+                        </div>
+                        <div className="office-card">
+                            <img
+                                src={christi}
+                                alt="Christi"
+                            />
+                            <div className="info">
+                                <strong>Christi Du Plessis</strong>
+                                <h5><em>Interior designer</em></h5>
+                                <p>National Certificate in Interior design TUT (Tshwane University of Technology) – 2005</p>
+                                <p>Loves: Making a difference</p>
+                                <p>Superpowers: Endurance</p>
+                            </div>
+                        </div>
+                        <div className="office-card">
+                            <img
+                                src={gunther}
+                                alt="Gunther"
+                            />
+                            <div className="info">
+                                <strong>Christi Du Plessis</strong>
+                                <h5><em>Interior designer</em></h5>
+                                <p>National Certificate in Interior design TUT (Tshwane University of Technology) – 2005</p>
+                                <p>Loves: Making a difference</p>
+                                <p>Superpowers: Endurance</p>
+                            </div>
+                        </div>
+                        <div className="office-card">
+                            <img
+                                src={kulsum}
+                                alt="Kulsum"
+                            />
+                            <div className="info">
+                                <strong>Kulsum Dickson</strong>
+                                <h5><em>Office manager</em></h5>
+                                <p>Loves: Helping others and spending time with family</p>
+                                <p>Superpowers: Brews the best cappuccinos ever</p>
+                            </div>
+                        </div>
+                        <div className="office-card">
+                            <img
+                                src={lumari}
+                                alt="Lumari"
+                            />
+                            <div className="info">
+                                <strong>Lumari Pretorius</strong>
+                                <h5><em>Architect and interior designer</em></h5>
+                                <p>National Certificate in Interior design TUT (Tshwane University of Technology) – 2005</p>
+                                <p>Loves: Learning new things, designing and going on adventures</p>
+                                <p>Superpowers: Dynamite comes in small package</p>
+                            </div>
                         </div>
                     </main>
                 </React.Fragment>
