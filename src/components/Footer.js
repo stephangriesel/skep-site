a4e62d5e5560c301e2a86c06d48d1fd2a4f26633
Stephan <sgriesel@gmail.com>
2019-09-22 07:25:54 +0200

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,24 +7,18 @@ class Footer extends Component {
             <div className="footer">
                 <div className="btm-menu">
                     <ul>
-                        <li>
-                            <a href="">Work</a>
-                        </li>
-                        <li>
-                            <a href="">Office</a>
-                        </li>
-                        <li>
-                            <a href="">Shop</a>
-                        </li>
-                        <li>
-                            <a href="">Contact</a>
-                        </li>
+                        <li><a href="/">WORK</a></li>
+                        <li><a href="office">OFFICE</a></li>
+                        <li><a href="shop">SHOP</a></li>
+                        <li><a href="contact">CONTACT</a></li>
                     </ul>
                 </div>
                 <div className="btm-icons">
                     <p><i className="far fa-copyright"></i> 2019 SKEP</p>
-                    <div>
-                        <i className="fab fa-instagram icon"></i>
+                    <div className="btm-menu">
+                        <a href="https://www.instagram.com/skepstudio">
+                            <i className="fab fa-instagram icon"></i>
+                        </a>
                     </div>
                 </div>
             </div>
