a4a6684b2c6113170b0d03ceb0485441da5c6a04
Stephan <sgriesel@gmail.com>
2020-02-22 15:58:46 +0100

    remove contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,36 +2,36 @@ import React, { Component } from 'react';
 import '../css/Contact.css';
 
 class Contact extends Component {
-    render() {
-        return (
-            <div className="center-wrapper">
-                <header className="contact-contact">
-                    <h1>Contact</h1>
-                </header>
-                <form method="post">
-                    <input type="hidden" name="form-name" value="contact" />
-                    <p>
-                        <label>
-                            <input placeholder="Please enter your name" type="text" name="name" />
-                        </label>
-                    </p>
-                    <p>
-                        <label>
-                            <input placeholder="Your email" type="email" name="email" />
-                        </label>
-                    </p>
-                    <p>
-                        <label>
-                            <textarea placeholder="Your message" name="message" />
-                        </label>
-                    </p>
-                    <p>
-                        <button type="submit">Send</button>
-                    </p>
-                </form>
-            </div>
-        )
-    }
+    // render() {
+    //     return (
+    //         <div className="center-wrapper">
+    //             <header className="contact-contact">
+    //                 <h1>Contact</h1>
+    //             </header>
+    //             <form method="post">
+    //                 <input type="hidden" name="form-name" value="contact" />
+    //                 <p>
+    //                     <label>
+    //                         <input placeholder="Please enter your name" type="text" name="name" />
+    //                     </label>
+    //                 </p>
+    //                 <p>
+    //                     <label>
+    //                         <input placeholder="Your email" type="email" name="email" />
+    //                     </label>
+    //                 </p>
+    //                 <p>
+    //                     <label>
+    //                         <textarea placeholder="Your message" name="message" />
+    //                     </label>
+    //                 </p>
+    //                 <p>
+    //                     <button type="submit">Send</button>
+    //                 </p>
+    //             </form>
+    //         </div>
+    //     )
+    // }
 }
 
 export default Contact;
